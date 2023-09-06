8fc70fbb8de0e2c363649e954e004884fa773833
cksthf3211 <cksthf321@gmail.com>
2023-09-06 22:21:23 +0900

    뉴스내용가져옴

--- 크롤링/기본/03.뉴스제목과링크가져오기.py
+++ 크롤링/기본/03.뉴스제목과링크가져오기.py
@@ -1,3 +1,4 @@
+import requests
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
@@ -12,7 +13,7 @@ chrome_options.add_experimental_option("detach", True)
 # C드라이버에 경로 넣지 않아야함
 driver = webdriver.Chrome(options=chrome_options)
 
-url = "https://www.naver.com/"
+url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90"
 
 # 네이버 서버에 대화 시도
 driver.get(url)
@@ -23,11 +24,8 @@ time.sleep(5)
 # 네이버에서 html 줌
 html = driver.page_source
 
-# html 번역선생님으로 수프만듬 / 파싱
 soup = BeautifulSoup(html, 'html.parser')
 
-# class 선택자
-word = soup.select_one("a.ContentHeaderView-module__tab_text___IuWnG")
+links = soup.select('.news_tit')
 
-# 텍스트 요소만 출력
-print(word.text)
+print(links)
